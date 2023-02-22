ffe8e03b8aa295fee029e1b60f1764cc9b63f921
Danielle Souza <dany.sa1@hotmail.com>
2023-02-22 15:45:37 -0300

    feat: remove comments

--- src/main.tsx
+++ src/main.tsx
@@ -22,9 +22,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
